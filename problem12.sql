18c4b59271d5814755a7f15ecfc0a4bd7362ca11
Terry Schmidt <terryschmidt@gmail.com>
2015-03-11 19:39:03 -0500

    Create problem12.sql

---
+++ problem12.sql
@@ -0,0 +1,9 @@
+--Use INTERSECT to list the vendors supplying a line item containing 'CICS' and a line item containing 'MVS'. Include the vendor ID and name.
+
+select vendors.vendor_id, vendors.vendor_name
+from invoice_line_items, invoices, vendors
+where invoice_line_items.LINE_ITEM_DESCRIPTION like '%CICS%' AND invoices.invoice_id = invoice_line_items.invoice_id AND invoices.vendor_id = vendors.vendor_id
+INTERSECT
+select vendors.vendor_id, vendors.vendor_name
+from vendors, invoice_line_items, invoices
+where invoice_line_items.LINE_ITEM_DESCRIPTION like '%MVS%' AND invoices.invoice_id = invoice_line_items.invoice_id AND invoices.vendor_id = vendors.vendor_id;
